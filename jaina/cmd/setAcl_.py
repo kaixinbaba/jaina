6c24f6e63239fb790afb5faca87832eaa1e6b633
kaixinbaba <452914639@qq.com>
2020-09-30 22:03:54 +0800

    setAcl docs done

--- jaina/cmd/setAcl_.py
+++ jaina/cmd/setAcl_.py
@@ -1,10 +1,43 @@
+from optparse import OptionParser
+
 from cmd.common import Command
 
 
-class QuitCommand(Command):
+class SetAclCommand(Command):
+    """
+    [green]Set permission information of the specified path.[/green]
+    [white]Try '[bold]setAcl -h[/bold]' for more information about options.:smile:[/white]
+    [yellow]
+    scheme: world | ip | digest | super
+    type:
+        when scheme == 'world'  >> 'anynone'
+        when scheme == 'ip'     >> 'some ip address'
+        when scheme == 'digest' >> 'username:base64password'
+        when scheme == 'super'  >> like 'digest'
+    perm:
+        c   : Create
+        d   : Delete
+        r   : Read
+        w   : Write
+        a   : Admin
+        all : All above
+    [/yellow]
+    [blue]Example:
+    (jaina) \[/] setAcl /your/path scheme:type:perm
+    (jaina) \[/] setAcl -v 1 /your/path scheme:type:perm
+    (jaina) \[/] setAcl /your/path world:anyone:cdrwa
+    (jaina) \[/] setAcl /your/path ip:192.168.1.11:cwa
+    (jaina) \[/] setAcl /your/path ip:192.168.1.11/24:cd
+    (jaina) \[/] setAcl /your/path digest:<username>:<base64password>:r
+    [/blue]
+    """
 
     def __init__(self, name):
         super().__init__(name)
+        self.parser = OptionParser()
+        self.parser.add_option("-v", "--version",
+                               action="store", type=int, dest="version", default=-1,
+                               help="Version requirements of the target node, any if -1, default -1")
 
     def process(self, opt, arg, cli):
-        pass
+        print(opt, arg)
--- tests/test_kazoo1.py
+++ tests/test_kazoo1.py
@@ -1,9 +1,19 @@
 from kazoo.client import KazooClient
+from kazoo.security import ACL, make_acl
 
 client = KazooClient(hosts='127.0.0.1:2181')
 try:
     client.start()
-    r = client.get_acls('/test')
+    # world auth digest ip
+    scheme = ''
+    # anyone
+    # addauth digest user:pwd
+    # username:base64
+    #
+    ID = ''
+
+    r = client.set_acls('/test', [make_acl('world', 'anyone', read=True, write=True, admin=True)])
+    # r = client.get_acls('/test')
     print(r, type(r), len(r))
 finally:
     client.stop()
